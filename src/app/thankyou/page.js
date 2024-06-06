d5d866762cd6937138b1b464a2cc7b5b78a63bb3
Rusty Sams <rustysams@gmail.com>
2024-06-06 15:13:06 -0400

    Fixed Thankyou Page

--- src/app/thankyou/page.js
+++ src/app/thankyou/page.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const page = () => {
 	return (
-		<div>
-			Thank you for emailing us. We will contact you regarding your inquiry. If
-			you have any questions or would like to see some examples, you may visit
-			our engravings or headstone pages or if you are in the neighborhood, swing
-			by our location at 5887 Unit B, La Plata MD 20646.
+		<div className='mx-auto w-full max-w-[1040px] mt-16 p-8'>
+			<p className='border-2 border-slate-300 text-lg p-6'>
+				Thank you for emailing us. We will contact you regarding your inquiry.
+				If you have any questions or would like to see some examples, you may
+				visit our engravings or headstone pages or if you are in the
+				neighborhood, swing by our location at 5887 Unit B, La Plata MD 20646.
+			</p>
 		</div>
 	)
 }
